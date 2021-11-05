b6dbe5cceeb4d10f39aa3d73ba82c04a0ae27437
Devone Reynolds <gamedevone1@gmail.com>
2021-11-05 10:10:46 -0700

    Deleted syntax test for gdlinter

--- Ball.gd
+++ Ball.gd
@@ -1,8 +1,4 @@
 extends Area2D
-
-func ITwasNevermeantTOBe() -> int:
-	var There_is_an_Eighty_Character_Line_Limit_To_Prevent_Undreadable_Lines_Like_This_One: int = 9
-	return There_is_an_Eighty_Character_Line_Limit_To_Prevent_Undreadable_Lines_Like_This_One
 	
 func _ready():
 	pass # Replace with function body.
